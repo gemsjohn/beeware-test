0b8d8103e5b12de26d51fca395f5c2064ba47234
JRM <johnrhmartin@gmail.com>
2023-03-02 10:24:12 -0500

    completed BeeWare tutorial 4

--- helloworld/src/helloworld/app.py
+++ helloworld/src/helloworld/app.py
@@ -5,6 +5,12 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
+def greeting(name):
+    if name:
+        return f"Hello, {name}"
+    else:
+        return "Hello, stranger"
+
 
 class HelloWorld(toga.App):
 
@@ -43,7 +49,10 @@ class HelloWorld(toga.App):
         self.main_window.show()
 
     def say_hello(self, widget):
-        print(f"Hello, {self.name_input.value}")
+        self.main_window.info_dialog(
+            greeting(self.name_input.value),
+            "Hi there!",
+        )
 
 
 def main():
